5a39cdfd6f351f91a7ab9e71435e4adfc932af60
elokthewizard <bradencbateman@gmail.com>
2024-05-28 11:43:58 -0600

    init listing model and date logic

--- auctions/models.py
+++ auctions/models.py
@@ -1,15 +1,24 @@
+from datetime import timedelta
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
 
+def default_end_date():
+    return timezone.now() + timedelta(days=3)
+
 class Listing(models.Model):
     title = models.CharField(max_length=64)
-    body = models.TextField(max_length=256)
-    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(max_length=256)
+
+    start_date = models.DateTimeField(auto_now_add=True, blank=False)
+    end_date = models.DateTimeField(default=default_end_date)
+
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    minimum_bid = models.DecimalField(max_digits=10, decimal_places=2)
     current_offer = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
