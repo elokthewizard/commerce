48d1cfbc73364fc1f93b667cc778d374f0d53464
elokthewizard <bradencbateman@gmail.com>
2024-06-05 13:57:00 -0600

    init models

--- auctions/models.py
+++ auctions/models.py
@@ -11,15 +11,33 @@ def default_end_date():
     return timezone.now() + timedelta(days=3)
 
 class Listing(models.Model):
+    #todo: image path
+    # image = models.ImageField()
+
+    # setup one to many relationship to User class
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
     title = models.CharField(max_length=64)
     description = models.TextField(max_length=256)
-
-    start_date = models.DateTimeField(auto_now_add=True, blank=False)
-    end_date = models.DateTimeField(default=default_end_date)
 
     starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
     minimum_bid = models.DecimalField(max_digits=10, decimal_places=2)
     current_offer = models.DecimalField(max_digits=10, decimal_places=2)
 
+    start_date = models.DateTimeField(auto_now_add=True, blank=False)
+    end_date = models.DateTimeField(default=default_end_date)
+
     def __str__(self):
         return self.title
+    
+class Bid(models.Model):
+    user = models.CharField(max_length=32)
+    date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=32)
+    comment = models.TextField(max_length=128)
--- commerce/settings.py
+++ commerce/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
